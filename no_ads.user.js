ae22020e76059fddd3e02e421a01e6f81ffbf0c8
Alexander <lugovov@users.noreply.github.com>
2017-11-11 10:31:07 +0300

    Другой код декодирования ссылок на музыку

--- no_ads.user.js
+++ no_ads.user.js
@@ -2,7 +2,7 @@
 // @name        no ads & music download
 // @namespace   vk_downloader
 // @include     https://vk.com/*
-// @version     1.0.4
+// @version     1.0.5
 // @updateURL	https://github.com/lugovov/vk_ext/raw/master/no_ads.user.js
 // @grant       none
 // @noframes
@@ -25,79 +25,99 @@
     }
     return text.join('');
   };
-	var vk={};
-	
-	(function(t, e) {
-	"use strict";
-		function i() {
-			return window.wbopen && ~(window.open + "").indexOf("wbopen")
-		}
+	#from kenzo_vk https://github.com/icw82/kenzo-vk/blob/master/sources/ext/modules/audio/vk.js
+	const check_url = source => {
+        const map =
+            'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMN0PQRSTUVWXYZO123456789+/=';
 
-		function o(t) {
-			if (!i() && ~t.indexOf("audio_api_unavailable")) {
-				var e = t.split("?extra=")[1].split("#"),
-					o = "" === e[1] ? "" : a(e[1]);
-				if (e = a(e[0]), "string" != typeof o || !e) return t;
-				o = o ? o.split(String.fromCharCode(9)) : [];
-				for (var s, r, n = o.length; n--;) {
-					if (r = o[n].split(String.fromCharCode(11)), s = r.splice(0, 1, e)[0], !l[s]) return t;
-					e = l[s].apply(null, r)
-				}
-				if (e && "http" === e.substr(0, 4)) return e
-			}
-			return t
-		}
+        const transforms = {
+            v: function (t) {
+//                console.log('V');
+                return t.split('').reverse().join('')
+            },
+            r: function (t, e) {
+//                console.log('R');
+                t = t.split('');
+                for (var i, o = map + map, a = t.length; a--;)
+                    i = o.indexOf(t[a]), ~i && (t[a] = o.substr(i - e, 1));
+                return t.join('')
+            },
+            s: (string, number) => {
+//                console.log('S');
+                const size = string.length;
+                if (size) {
+                    var o = hueta(string, number);
+                    var a = 0;
+                    for (string = string.split(''); ++a < size;) {
+                        string[a] = string.splice(
+                            o[size - 1 - a], 1, string[a]
+                        )[0];
+                    }
+                    string = string.join('')
+                }
+                return string;
+            },
+            x: function (t, e) {
+//                console.log('X');
+                var i = [];
+                return e = e.charCodeAt(0), each(t.split(''), function (t, o) {
+                    i.push(String.fromCharCode(o.charCodeAt(0) ^ e))
+                }), i.join('')
+            }
+        }
 
-		function a(t) {
-			if (!t || t.length % 4 == 1) return !1;
-			for (var e, i, o = 0, a = 0, s = ""; i = t.charAt(a++);) i = r.indexOf(i), ~i && (e = o % 4 ? 64 * e + i : i, o++ % 4) && (s += String.fromCharCode(255 & e >> (-2 * o & 6)));
-			return s
-		}
+        {
+            let extra = source.split('?extra=')[1].split('#');
+            let ugliness = '' === extra[1] ? '' : hut(extra[1]);
 
-		function s(t, e) {
-			var i = t.length,
-				o = [];
-			if (i) {
-				var a = i;
-				for (e = Math.abs(e); a--;) o[a] = (e += e * (a + i) / e) % i | 0
-			}
-			return o
-		}
-		Object.defineProperty(e, "__esModule", {
-			value: !0
-		}), e.audioUnmaskSource = o;
-		var r = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMN0PQRSTUVWXYZO123456789+/=",
-			l = {
-				v: function(t) {
-					return t.split("").reverse().join("")
-				},
-				r: function(t, e) {
-					t = t.split("");
-					for (var i, o = r + r, a = t.length; a--;) i = o.indexOf(t[a]), ~i && (t[a] = o.substr(i - e, 1));
-					return t.join("")
-				},
-				s: function(t, e) {
-					var i = t.length;
-					if (i) {
-						var o = s(t, e),
-							a = 0;
-						for (t = t.split(""); ++a < i;) t[a] = t.splice(o[i - 1 - a], 1, t[a])[0];
-						t = t.join("")
-					}
-					return t
-				},
-				x: function(t, e) {
-					var i = [];
-					return e = e.charCodeAt(0), each(t.split(""), function(t, o) {
-						i.push(String.fromCharCode(o.charCodeAt(0) ^ e))
-					}), i.join("")
-				}
-			}
-})(0, vk);
+            extra = hut(extra[0]);
+
+            if (typeof ugliness != 'string' || !extra)
+                return source;
+
+            ugliness = ugliness ? ugliness.split(String.fromCharCode(9)) : [];
+
+            for (let s, r, n = ugliness.length; n--;) {
+                r = ugliness[n].split(String.fromCharCode(11));
+                s = r.splice(0, 1, extra)[0];
+                if (!transforms[s])
+                    return source;
+                extra = transforms[s].apply(null, r)
+            }
+
+            if (extra && 'http' === extra.substr(0, 4))
+                return extra
+        }
+
+        function hut(kusok) {
+            if (!kusok || kusok.length % 4 == 1)
+                return !1;
+            for (var e, i, o = 0, a = 0, s = ''; i = kusok.charAt(a++);) {
+                i = map.indexOf(i);
+                ~i &&
+                (e = o % 4 ? 64 * e + i : i, o++ % 4) &&
+                (s += String.fromCharCode(255 & e >> (-2 * o & 6)));
+            }
+            return s
+        }
+
+        function hueta(string, number) {
+            const size = string.length;
+            const array = [];
+            if (size) {
+                let count = size;
+                for (number = Math.abs(number); count--;) {
+                    array[count] =
+                        (number += number * (count + size) / number) % size | 0
+                }
+            }
+            return array
+        }
+    }
 	
   var className=makeid(16+Math.ceil(Math.random()*16));
     var getLink=function(t){
-	    return vk.audioUnmaskSource(t)
+	    return check_url(t)
 };
   var updateContent=function(element){
     try{
